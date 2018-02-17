274864a4ce505746fa3fd168066cdd672f3953dd
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-02-17 15:39:18 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,4 +3,3 @@ def simple_app(environ, start_response):
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return [environ['QUERY_STRING'].replace('&', '\n')]
- 
