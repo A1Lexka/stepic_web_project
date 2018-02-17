3f4e781ed4a88dbff1108ee0b1757bf3d855cde8
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-02-17 15:39:00 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
-def app(environ, start_response):
-  data = b"Hello, World!\n"
-  start_response("200 OK", [
-  ("Content-Type", "text/plain"),
-  ("Content-Length", str(len(data)))
-  ])
-  return iter([data])
+def simple_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return [environ['QUERY_STRING'].replace('&', '\n')]
  
