857d4cf379beee473c848b9bc182fa2311ea47a2
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-02-18 17:26:48 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
 sudo /etc/init.d/gunicorn restart
+sudo unlink /etc/gunicorn.d/django
+sudo ln -s /home/box/web/etc/django /etc/gunicorn.d/django
+sudo /etc/init.d/gunicorn restart
