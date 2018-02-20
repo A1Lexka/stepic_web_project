01982cacca707d71c4fe11c89b571baf7afe2ec4
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-02-20 23:53:01 +0500

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from qa.views import test, main_page, question_page, popular_questions, ask_page, signup, login_page
+from qa.views import test
 
 urlpatterns = [ 
     url(r'^$', 'test'),                                                              
