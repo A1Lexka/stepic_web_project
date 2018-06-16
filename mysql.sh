b47b705226637b06cc750d81ead25ecfed759aff
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-06-16 22:56:40 +0500

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,2 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE blog"
-mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass123';"
-mysql -uroot -e "GRANT ALL ON blog.* TO 'django@localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
