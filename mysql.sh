eb3d7ee3df0ba94817e4b5a55cc86e7068147e56
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-06-17 15:40:04 +0500

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,2 @@
 sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE USER 'admin'@'localhost'"
-mysql -uroot -e "SET PASSWORD FOR 'admin'@'localhost' = PASSWORD('pass111')"
-mysql -uroot -e "CREATE DATABASE mybase"
-mysql -uroot -e "GRANT ALL ON mybase.* TO 'admin'@'localhost'"
+mysql -uroot -e "CREATE USER blog"
