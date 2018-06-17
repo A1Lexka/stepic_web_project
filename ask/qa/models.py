a0f8b63e89e4f16bfec8abcc810ecc2041a783ad
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-06-17 15:41:39 +0500

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
-from datetime import timedelta
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class QuestionManager(models.Manager):                                          
