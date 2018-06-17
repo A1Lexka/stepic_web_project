bce00aca1b6e0f62fba29f07cf91095614ae2376
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-06-17 15:54:31 +0500

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -11,12 +11,12 @@ class QuestionManager(models.Manager):
         return self.order_by('-rating')
 
 class Question(models.Model):
-    title = models.CharField(max_length=225)
-    text = models.TextField()
+    title = models.CharField(default="", max_length=225)
+    text = models.TextField(default="")
     added_at = models.DateTimeField(blank = True, auto_now_add=True)
     rating = models.IntegerField(default = 0)
-    author = models.CharField(max_length=20)
-    likes = models.models.ForeignKey(User, null=True)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name="q_to_likes")
     objects = QuestionManager()
     def __unicode__(self):
           return self.title
@@ -24,8 +24,8 @@ class Question(models.Model):
           return "/question/{}".format(self.id)
     
 class Answer(models.Model):
-    text = models.TextField()
-    added_at = models.DateTimeField(blank = True, auto_now_add=True)
+    text = models.TextField(default="")
+    added_at = models.DateTimeField(null=True, auto_now_add=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     def __str__(self):
