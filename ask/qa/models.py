30dfc34afe6d637bcd435717d55585a9af9d100f
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-06-17 15:44:14 +0500

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -15,8 +15,8 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank = True, auto_now_add=True)
     rating = models.IntegerField(default = 0)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name="q_to_likes")
+    author = models.CharField(max_length=20)
+    likes = models.models.ForeignKey(User, null=True)
     objects = QuestionManager()
     def __unicode__(self):
           return self.title
