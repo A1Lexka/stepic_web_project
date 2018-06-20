7ba1da4543635fa524aa34e79e8b5a11b5470d04
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-06-20 20:32:10 +0500

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,85 +1,101 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 from django.core.paginator import Paginator
 from django.contrib.auth import authenticate, login
 
-from qa.models import Question, Answer
-# Create your views here.
+
+from django.views.decorators.http import require_GET, require_POST, require_http_methods
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
+from models import Question
+from forms import AskForm, AnswerForm, RegisterForm
 
 
-def question(request, num,):
-    try:
-        q = Question.objects.get(id=num)
-    except Question.DoesNotExist:
-        raise Http404
-    if request.method == "POST":
-        form = AnswerForm(request.POST)
-        if form.is_valid():
-            form._user = request.user
-            _ = form.save()
-            url = q.get_url()
-            return HttpResponseRedirect(url)
-    else:
-        form = AnswerForm(initial={'question': q.id})
-
-    return render(request, 'question.html', {'question': q,
-                                             'form': form,
-                                             'user': request.user,
-                                             'session': request.session, })
-
-
-def index(request):
-    try:
-        page = int(request.GET.get("page"))
-    except ValueError:
-        page = 1
-    except TypeError:
-        page = 1
-    questions = Question.objects.all().order_by('-id')
-    paginator = Paginator(questions, 10)
-    page = paginator.page(page)
-
-    return render(request, 'list.html',
-                  {'title': 'Latest',
-                   'paginator': paginator,
-                   'questions': page.object_list,
-                   'page': page,
-                   'user': request.user,
-                   'session': request.session, })
-
-
-def popular(request):
-    try:
-        page = int(request.GET.get("page"))
-    except ValueError:
-        page = 1
-    except TypeError:
-        page = 1
-    questions = Question.objects.all().order_by('-rating')
-    paginator = Paginator(questions, 10)
-    page = paginator.page(page)
-
-    return render(request, 'list.html',
-                  {'title': 'Popular',
-                   'paginator': paginator,
-                   'questions': page.object_list,
-                   'page': page,
-                   'user': request.user,
-                   'session': request.session, })
-
-
+@require_http_methods(['GET', 'POST'])
+# @login_required(login_url='/login')
 def ask(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = AskForm(request.POST)
+        form._user = request.user
         if form.is_valid():
-            form._user = request.user
-            post = form.save()
-            url = post.get_url()
-            return HttpResponseRedirect(url)
+            q = form.save()
+            return HttpResponseRedirect('/question/%d' % q.id)
+
     else:
         form = AskForm()
-    return render(request, 'ask.html', {'form': form,
-                                        'user': request.user,
-                                        'session': request.session, })
+
+    return render(request, 'qa/ask.html', {'form': form})
+
+
+@require_POST
+# @login_required(login_url='/login')
+def answer(request):
+    form = AnswerForm(request.POST)
+    form._user = request.user
+    if form.is_valid():
+        q_id = form.cleaned_data['question']
+        form.save()
+        return HttpResponseRedirect('/question/%d' % q_id)
+
+
+@require_GET
+def home(request):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 1))
+    except:
+        page = 1
+    paginator = Paginator(Question.objects.all(), limit)
+    questions = paginator.page(page)
+    context = {'questions': questions}
+    return render(request, 'qa/index.html', context)
+
+
+@require_GET
+def popular(request):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 1))
+    except:
+        page = 1
+    paginator = Paginator(Question.objects.order_by('-rating'), limit)
+    questions = paginator.page(page)
+    context = {'questions': questions}
+    return render(request, 'qa/popular.html', context)
+
+
+def q_id(request, q_id):
+    question = get_object_or_404(Question, id=q_id)
+    form = AnswerForm(initial={'question': q_id})
+    context = {'question': question,
+               'form': form}
+    return render(request, 'qa/q_detail.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+            return HttpResponseRedirect('/')
+
+    else:
+        form = RegisterForm()
+
+    return render(request, 'qa/signup.html', {'form': form})
