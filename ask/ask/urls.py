4092bb379a18dbc6575e89b627d709e6cf4c3dc1
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-06-20 22:36:48 +0500

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -20,13 +20,13 @@ from qa.views import test
 
 urlpatterns = [
     url(r'^$', test), 
-    url(r'^$', 'qa.views.home', name='home'),                                                              
+    url(r'^$', qa.views.home, name='home'),                                                              
     url(r'^login/.*$', test, name='login'),                                    
     url(r'^signup/.*$', test, name='signup'), 
-    url(r'^popular/', 'qa.views.popular', name='popular'),                              
+    url(r'^popular/', qa.views.popular, name='popular'),                              
     url(r'^new/.*', test, name='new'),
-    url(r'^ask/', 'qa.views.ask', name='ask'),
-    url(r'^answer/', 'qa.views.answer', name='answer'),
-    url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),
+    url(r'^ask/', qa.views.ask, name='ask'),
+    url(r'^answer/', qa.views.answer, name='answer'),
+    url(r'^question/(?P<id>[0-9]+)/$', qa.viwes.question, name='question'),
     url(r'^admin/', admin.site.urls),
 ]
