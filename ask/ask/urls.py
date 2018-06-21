12977c3131e7a53d3c9b3a31c1568ee45a487398
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-06-21 22:51:40 +0500

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,19 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from qa.views import test
-from qa.views import home
-from qa.views import popular
-from qa.views import question
 
 urlpatterns = [
     url(r'^$', test), 
-    url(r'^$', qa.views.home, name='home'),                                                              
-    url(r'^login/.*$', test, name='login'),                                    
     url(r'^signup/.*$', test, name='signup'), 
-    url(r'^popular/', qa.views.popular, name='popular'),                              
+    url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),
+    url(r'^ask/.*$', test, name='ask'),
+    url(r'^popular/.*', test, name='popular'),
     url(r'^new/.*', test, name='new'),
-    url(r'^ask/', qa.views.ask, name='ask'),
-    url(r'^answer/', qa.views.answer, name='answer'),
-    url(r'^question/(?P<id>[0-9]+)/$', qa.viwes.question, name='question'),
     url(r'^admin/', admin.site.urls),
 ]
