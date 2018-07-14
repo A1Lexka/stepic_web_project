63463d58e60d573b4ae6b523a83f035debd8c3e7
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-07-14 20:52:59 +0500

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -5,5 +5,46 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
   
   # Create your views here.
+
+def index(request):
+    try:
+        page = int(request.GET.get("page"))
+    except ValueError:
+        page = 1
+    except TypeError:
+        page = 1
+    questions = Question.objects.all().order_by('-id')
+    paginator = Paginator(questions, 10)
+    page = paginator.page(page)
+    return render(request, 'qa/index.html', context)
+
+def popular(request):
+    try:
+        page = int(request.GET.get("page"))
+    except ValueError:
+        page = 1
+    except TypeError:
+        page = 1
+    questions = Question.objects.all().order_by('-rating')
+    paginator = Paginator(questions, 10)
+    page = paginator.page(page)
+    return render(request, 'qa/popular.html', context)
+
+def question(request, num,):
+    try:
+        q = Question.objects.get(id=num)
+    except Question.DoesNotExist:
+        raise Http404
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            form._user = request.user
+            _ = form.save()
+            url = q.get_url()
+            return HttpResponseRedirect(url)
+    else:
+        form = AnswerForm(initial={'question': q.id})
+    return render(request, 'qa/'question.html', context)
+
   
   
