ebe61ceb9f556ed341a13661f8ffea965d5e5910
A1Lexka <36447134+A1Lexka@users.noreply.github.com>
2018-07-14 23:00:14 +0500

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from qa.models import Question, Answer
 
 from django.http import HttpResponse 
 def test(request, *args, **kwargs):
